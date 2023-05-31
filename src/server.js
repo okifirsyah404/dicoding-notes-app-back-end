3517b8f3f4d10b0262c3ae304b742648f78a5cff
okifirsyah404 <85375019+okifirsyah404@users.noreply.github.com>
2023-05-31 20:54:01 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
+/* eslint-disable object-curly-spacing */
+const Hapi = require('@hapi/hapi');
+const { routes } = require('./routes.js');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host: 'localhost',
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
+	});
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+
+	server.route(routes);
+};
+
+init();
